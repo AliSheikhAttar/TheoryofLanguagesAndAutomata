f3d32a529002a0aa2079f0a5edc6b13e9918b5bf
AliSheikh <ali.sheikhattar@yahoo.com>
2023-05-09 15:19:43 +0200

    part1&auxiliary

--- Project1/part1/part1.py
+++ Project1/part1/part1.py
@@ -1,7 +1,7 @@
-import json
+import json, sys
 from graphviz import Digraph
-from auxiliaries import *
 
+from auxiliaries import *
 
 
 class Node1 :
@@ -19,9 +19,6 @@ class Node1 :
         self.name1.sort(key=lambda x: int(x[1]))
         for n in self.name1:
             self.name += n
-
-
-
 
 
 
@@ -60,7 +57,6 @@ def Start(start):
     return repeatd(newstates)
 
 
-newstates = [Start(starting_node)]
 def convert( states):
     for alphabet in alphabets :
         newstate = []
@@ -80,92 +76,6 @@ def convert( states):
 
 
 
-
-# convert(newstates[0]) 
-# print(newstates)
- 
-
- 
-
-def convert2json_DFA(states, input_symbols, starting_node, fs):
-    states_str = "{"
-    input_symbols_str = "{"
-    final_states_str = "{"
-    for state in states:
-        states_str += f"'{state.name}',"
-    for input_symbol in input_symbols:
-        input_symbols_str += f"'{input_symbol}',"
-    for final_state in fs:
-        final_states_str += f"'{final_state.name}',"
-    items = [states_str, input_symbols_str, final_states_str]
-    for i in range(len(items)):
-        items[i] = items[i][:-1]
-        items[i]+="}"
-    transitions_str = {}
-    for state in states:
-        transitions_str[f"{state.name}"] = {}
-        for input_symbol in input_symbols:
-            if(len(state.actions[input_symbol])==0): 
-                transitions_str[f"{state.name}"][f"{input_symbol}"] = "TRAP"
-            else:
-                transitions_str[f"{state.name}"][f"{input_symbol}"] = str(state.actions[input_symbol][0].name)
-
-    dictionary = {"states":items[0], "input_symbol": items[1],
-                 "transition":transitions_str,
-                 "initial_state":str(starting_node.name), "final_states":items[2]}
-
-    jsonFile = json.dumps(dictionary,indent=4)
-    return jsonFile
-     
-
-
-def convert2json_NFA(states, input_symbols, starting_node, fs):
-    states_str = "{"
-    input_symbols_str = "{"
-    final_states_str = "{"
-    for state in states:
-        states_str += f"'{state.name}',"
-    for input_symbol in input_symbols:
-        input_symbols_str += f"'{input_symbol}',"
-    for final_state in fs:
-        final_states_str += f"'{final_state.name}',"
-    items = [states_str, input_symbols_str, final_states_str]
-    for i in range(len(items)):
-        items[i] = items[i][:-1]
-        items[i]+="}"
-    transitions_str = {}
-    for state in states:
-        transitions_str[f"{state.name}"] = {}
-        for key,value in state.actions.items():
-            if(key=="$"):
-                alphabet = ""
-            else:
-                alphabet = key
-            if(len(value)!=0): 
-                transitions_str[f"{state.name}"][f"{alphabet}"] = "{"
-                if(len(value)==1):
-                    transitions_str[f"{state.name}"][f"{alphabet}"] += f"'{value[0].name}'"
-                    transitions_str[f"{state.name}"][f"{alphabet}"] += "}"
-                else:
-                    for i in range(len(value)):
-                        transitions_str[f"{state.name}"][f"{alphabet}"] += f"'{value[i].name}',"
-                    transitions_str[f"{state.name}"][f"{alphabet}"] = transitions_str[f"{state.name}"][f"{alphabet}"][:-1]
-                    transitions_str[f"{state.name}"][f"{alphabet}"] += "}"
-
-
-
-
-    dictionary = {"states":items[0], "input_symbol": items[1],
-                 "transition":transitions_str,
-                 "initial_state":str(starting_node.name), "final_states":items[2]}
-
-    jsonFile = json.dumps(dictionary,indent=4)
-    return jsonFile
-
-
-
-result = newstates
-
 def listtonode(result):
     ListOfnode = []
     for i in range(len(result)):
@@ -182,7 +92,8 @@ def listtonode(result):
             ListOfnode.append(toappend)
     return ListOfnode
 
-listofnode = listtonode(result)
+
+
 
 def find_node2(namelist,nodes):
     for node in nodes :
@@ -205,6 +116,14 @@ def addactions(result,listofnodes):
             here_node.add_action(a,findstate)
 
 
+
+
+address = 'C:\git\TheoryofLanguagesAndAutomata\Project1\samples\phase1-sample\in\input1.json'
+alphabets, nodes, starting_node, fs = json2code(address)
+newstates = [Start(starting_node)]
+convert(newstates[0])
+result = newstates
+listofnode = listtonode(result)
 lr = addactions(result,listofnode)
 
 for node in listofnode:
@@ -217,12 +136,12 @@ for node in listofnode:
 fss = []
 for i in range(len(listofnode)):
     for s in listofnode[i].name1:  
-        if(find_node(s) in fs):
+        if(find_node(s,nodes) in fs):
             fss.append(listofnode[i])
             break
 
 starting_node = listofnode[0]
 json1 = convert2json_DFA(listofnode,alphabets,starting_node,fss)
-print(json1)
+
 with open("output1.json", "w") as outfile:
     outfile.write(json1)
