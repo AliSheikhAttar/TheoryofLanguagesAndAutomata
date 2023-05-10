627c6478e0bfbb49ecf018b1620baa38b6ff79ca
saba <sabakianoosh81@gmail.com>
2023-05-10 11:47:56 +0430

    update part1

--- Project1/part4/star.py
+++ Project1/part4/star.py
@@ -1,15 +1,18 @@
 import json, sys, os
 from graphviz import Digraph
 SCRIPT_DIR = os.path.dirname(os.path.abspath('auxiliaries.py'))
-sys.path.append(os.path.dirname('C:\git\TheoryofLanguagesAndAutomata\Project1\auxiliaries.py'))
+sys.path.append(os.path.dirname('C:\git\TheoryofLanguagesAndAutomata\Project1\\auxiliaries.py'))
 
-from Project1.auxiliaries import *
+from auxiliaries import *
 
 states_ad = 'C:\git\TheoryofLanguagesAndAutomata\Project1\samples\phase4-sample\star\in\FA.json'
 alphabets, nodes, starting_state, fs = json2code(states_ad)
 
 for s in fs:
     s.add_action("$", starting_state)
+
+starting_state.add_action("$",fs[0])
+
 
 result = convert2json_NFA(nodes, alphabets, starting_state, fs)
 with open("output3.json", "w") as outfile:
